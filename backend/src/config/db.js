3bfcedfbb00149c82c11d51fa828b3221569962a
pal-kamlesh <kamleshiit5@gmail.com>
2025-04-30 23:41:26 +0530

    connection string changed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
