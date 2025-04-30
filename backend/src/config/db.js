e23a389a0cb98098b1c824ca81e654484b68dea4
pal-kamlesh <kamleshiit5@gmail.com>
2025-05-01 00:48:54 +0530

    some errror fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log(`PORT: ${process.env.PORT}`);
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
